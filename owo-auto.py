f7a2e10b1e3478d7ff4050c7bd639e6dcad50069
RamakantBot <ramakantbot@duck.com>
2024-11-30 23:47:14 +0530

    modified:   owo-auto.py

--- owo-auto.py
+++ owo-auto.py
@@ -1,32 +1,36 @@
 import time
 import pyautogui
 
-# -------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------
 
 def main():
-    print("press 'Ctrl+c' to stop the code.")
-    time.sleep(5)
+    print( i, ". Press 'Ctrl+c' to stop the code.")
+    time.sleep(t)
     custom()
-    time.sleep(5)
+    time.sleep(t)
     hunt()
-    time.sleep(5)
+    time.sleep(t)
     battle()
 
 # -------------------------------------------------------------------------------
 
 def custom():
-    pyautogui.write(str("rp"), interval = 0.1)
+    pyautogui.write(str("rp"), interval = 0.2)
     pyautogui.press('enter')
 
 def hunt():
-    pyautogui.write(str("owo h"), interval = 0.1)
+    pyautogui.write(str("owo h"), interval = 0.2)
     pyautogui.press('enter')
 
 def battle():
-    pyautogui.write(str("owo b"), interval = 0.1)
+    pyautogui.write(str("owo b"), interval = 0.2)
     pyautogui.press('enter')
 
 # -------------------------------------------------------------------------------
 
+i = 1
+t = 5
+
 while True:
     main()
+    i = i + 1
