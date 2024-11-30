f658aab9b2c4fa518b485da665542ecdc122b1d7
RamakantBot <ramakantbot@duck.com>
2024-11-30 23:22:40 +0530

    modified:   owo-auto.py

--- owo-auto.py
+++ owo-auto.py
@@ -5,16 +5,17 @@ import pyautogui
 
 def main():
     print("press 'Ctrl+c' to stop the code.")
-    time.sleep(7.5)
+    time.sleep(5)
     custom()
+    time.sleep(5)
     hunt()
-    time.sleep(7.5)
+    time.sleep(5)
     battle()
 
 # -------------------------------------------------------------------------------
 
 def custom():
-    pyautogui.write(str("w"), interval = 0.1)
+    pyautogui.write(str(":P"), interval = 0.1)
     pyautogui.press('enter')
 
 def hunt():
